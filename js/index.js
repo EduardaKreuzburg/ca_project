64f70c71a7a88cca616bdedb1c12017ce6906ec6
Eduarda Kreuzburg <dukreuzburg@gmail.com>
2020-12-20 15:57:16 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,44 +1,42 @@
-var raiz = document.querySelector(".raiz");
-var esquerda = false;
-var direita = false;
-// var centro = true;
+const raiz = document.querySelector(".raiz");
+const left_button = document.querySelector(".seta_left");
+const right_button = document.querySelector(".seta_right");
 
-function mover_direita() {
-    if (!direita) {
-        raiz.classList.add("movendo_esquerda")
-        raiz.classList.remove("movendo_esq_centro")
+const centrals = document.querySelectorAll(".central");
 
-        raiz.classList.remove("movendo_direita")
-        raiz.classList.remove("movendo_dir_centro")
+var pos = 1;
 
-        direita=true;
-    } else {
-        raiz.classList.add("movendo_esq_centro")
-        raiz.classList.remove("movendo_esquerda")
+// centrals[0].style = "display: none";
+// centrals[1].style = "display: none";
 
-        raiz.classList.remove("movendo_direita")
-        raiz.classList.remove("movendo_dir_centro")
+function mover(mv_bt) {
+    let pos_x = "-25%";
+    let button_display = "flex";
+    let central_display = "flex";
 
-        direita=false;
-    }
-}
-
-function mover_esquerda() {
-    if (!esquerda) {
-        raiz.classList.add("movendo_direita")
-        raiz.classList.remove("movendo_dir_centro")
-
-        raiz.classList.remove("movendo_esq_centro")
-        raiz.classList.remove("movendo_esquerda")
-
-        esquerda=true;
-    } else {
-        raiz.classList.add("movendo_dir_centro")
-        raiz.classList.remove("movendo_direita")
-
-        raiz.classList.remove("movendo_esq_centro")
-        raiz.classList.remove("movendo_esquerda")
-
-        esquerda=false;
+    if (mv_bt==0) {
+        if (pos==1) {
+            raiz.style = "transition: transform 1500ms; transform: translateX(0%)";
+            left_button.style = "display: none";
+            centrals[0].style = "transition: transform 1500ms; transform: translateX(0%);";
+            pos=0;
+        }else if (pos==2) {
+            raiz.style = "transition: transform 1500ms; transform: translateX(-25%)";
+            right_button.style = "display: flex";
+            centrals[1].style = "transition: transform 1500ms; transform: translateX(100%);";
+            pos=1;
+        }
+    }else if(mv_bt==1){
+        if (pos==1) {
+            raiz.style = "transition: transform 1500ms; transform: translateX(-50%)";
+            right_button.style = "display: none";
+            centrals[1].style = "transition: transform 1500ms; transform: translateX(0%);";
+            pos = 2;
+        }else if(pos==0){
+            raiz.style = "transition: transform 1500ms; transform: translateX(-25%)";
+            left_button.style = "display: flex";
+            centrals[0].style = "transition: transform 1500ms; transform: translateX(-100%);";
+            pos = 1;
+        }
     }
 }
